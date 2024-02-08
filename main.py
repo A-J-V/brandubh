8fef82b658aa923d28025bdc4c9c505039e75e1c
AJV <alexander.valarus@gmail.com>
2024-02-07 21:29:07 -0600

    Refactor training method and integrate new Trainer class
    Optimized the training loop by extracting it into a new 'Trainer' class in trainers.py. This class automates the training iterations, data processing, and also includes an option to clean the game records post-training, making the training process more efficient and streamlined.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import ai_utils
 from core import *
 import curriculum_learning
 import episodes
+import trainers
 
 import os
 import torch
@@ -18,26 +19,18 @@ if __name__ == '__main__':
     print(f"Model has {sum(p.numel() for p in attacker.parameters() if p.requires_grad)} parameters")
     attacker.to(device)
     optimizer = torch.optim.Adam(attacker.parameters(), lr=0.0001)
-    for iteration in range(100):
-        winner_dict = {0: 0,
-                       1: 0}
-        for i in tqdm(range(2000)):
-            env = episodes.Standard(recorded=True)
-            terminal = env.play()
-            winner_dict[terminal] += 1
-        winrate = winner_dict[1] / (winner_dict[1] + winner_dict[0])
-        print(f"Attacker winrate: {winrate}")
-        attacker_winrate.append(winrate)
-        dataset = ai_utils.BrandubhDataset(data_path="./game_records", player=1)
-        dataloader = DataLoader(dataset=dataset,
-                                batch_size=1024,
-                                shuffle=True)
-        loss_fn = ai_utils.PPOLoss(c2=0.0001)
-        for epoch in range(3):
-            ai_utils.train_agent(attacker, loss_fn, device, dataloader, optimizer)
-
-        files = os.listdir("./game_records")
-        for file in files:
-            file_path = os.path.join("./game_records", file)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
+    trainer = trainers.Trainer(player=1,
+                               data_path="./game_records",
+                               batch_size=1024,
+                               attacker=attacker,
+                               defender=ai.RandomAI(player=0),
+                               loss_fn=ai_utils.PPOLoss(),
+                               num_iter=5,
+                               games_per_iter=1000,
+                               optimizer=optimizer,
+                               device=device,
+                               epochs_per_iter=2,
+                               mode='standard',
+                               nuke_records=True,
+                               )
+    trainer.train()
