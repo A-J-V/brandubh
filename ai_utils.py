4cf784305381483b47d2e29cb39087915f319b45
AJV <alexander.valarus@gmail.com>
2024-02-11 12:21:09 -0600

    Refactor GameRecorder class in ai_utils.py
    The GameRecorder class is now fundamentally different in operation. It is unused during gameplay and requires no manual input now; instead, after the game terminates, the terminal node is passed to the GameRecorder's extract() method, and the GameRecorder automatically traverses the game tree and extracts all necessary data from the tree. This is a much cleaner way to record games.

--- ai_utils.py
+++ ai_utils.py
@@ -9,76 +9,30 @@ import os
 class GameRecorder:
     """Helper class to record a game's data for later AI training."""
     def __init__(self):
-        self.player = []
         self.state = []
-        self.action_space = []
-        self.actions = []
-        self.action_probs = []
-        self.v_est_1 = []
-        self.v_est_0 = []
-        self.terminal = []
-        self.num_turns = 0
+        self.policy_target = []
+        self.value_target = []
+        self.player = []
         self.winner = None
 
-    def tick(self):
-        assert (len(self.state) ==
-                len(self.action_space) ==
-                len(self.actions) ==
-                len(self.action_probs) ==
-                len(self.player) ==
-                len(self.v_est_1) ==
-                len(self.v_est_0)), "Game Recording is incomplete"
-        self.num_turns += 1
-
-    def get_td_error(self, vt, vtp, player, winner):
-        """Given the value estimates for t and t+1, player, and winner, calculate TD error for the player."""
-        rewards = np.zeros_like(vt.values)
-        # Currently there are no draws. If player didn't win, then they lost.
-        if winner == player:
-            rewards[-1] = 1
+    def extract(self, node):
+        self.state.append(node.board.flatten())
+        self.player.append(node.player)
+        if node.is_terminal:
+            self.winner = node.winner
+            self.policy_target.append(np.zeros(24*7*7))
         else:
-            rewards[-1] = -1
-
-        td_error = vtp - vt + rewards
-        return td_error
-
-    def calculate_gae(self, td_error, gamma=0.99, lambda_=0.99):
-        # Calculate GAE
-        gae = []
-        gae_t = 0
-        for t in reversed(range(len(td_error))):
-            delta = td_error.iloc[t]
-            gae_t = delta + gamma * lambda_ * gae_t
-            gae.insert(0, gae_t)
-        return gae
+            self.policy_target.append(node.policy)
+        if node.parent is not None:
+            self.extract(node.parent)
 
     def record(self):
         state_df = pd.DataFrame(self.state)
         state_df.columns = [f'c_{i}' for i, _ in enumerate(state_df.columns)]
-        action_space_df = pd.DataFrame(self.action_space)
-        action_space_df.columns = [f'as_{i}' for i, _ in enumerate(action_space_df.columns)]
-        game_record = pd.concat([state_df, action_space_df], axis=1)
-        game_record['player'] = self.player
-        game_record['action_taken'] = self.actions
-        game_record['action_prob'] = self.action_probs
-        game_record['v_est_1'] = self.v_est_1
-        game_record['v_est_0'] = self.v_est_0
-        game_record['v_est_next_1'] = game_record['v_est_1'].shift(-1, fill_value=0)
-        game_record['v_est_next_0'] = game_record['v_est_0'].shift(-1, fill_value=0)
-        game_record['terminal'] = self.terminal
-        game_record['winner'] = self.winner
-        game_record['td_error_1'] = self.get_td_error(game_record['v_est_1'],
-                                                      game_record['v_est_next_1'],
-                                                      player=1,
-                                                      winner=self.winner,
-                                                      )
-        game_record['td_error_0'] = self.get_td_error(game_record['v_est_0'],
-                                                      game_record['v_est_next_0'],
-                                                      player=0,
-                                                      winner=self.winner,
-                                                      )
-        game_record['gae_1'] = self.calculate_gae(game_record['td_error_1'])
-        game_record['gae_0'] = self.calculate_gae(game_record['td_error_0'])
+        policy_df = pd.DataFrame(self.policy_target)
+        policy_df.columns = [f'p_{i}' for i, _ in enumerate(policy_df.columns)]
+        game_record = pd.concat([state_df, policy_df], axis=1)
+        game_record['value_target'] = np.where(np.array(self.player) == self.winner, 1, -1)
         return game_record
 
 
