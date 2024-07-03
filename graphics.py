8bd4d42ce83a5082f3579cc69983955180ee380f
AJV <alexander.valarus@gmail.com>
2024-07-03 10:25:39 -0500

    Update graphic dimensions and scaling
    Added numerous scaling factors so that graphics are dynamically adjusted depending on window size.

--- graphics.py
+++ graphics.py
@@ -1,23 +1,32 @@
 import numpy as np
 import pygame
 
-
-WIN_SIZE = 700
-TILE_SIZE = 110
-RED_HIGHLIGHT = (240, 50, 50, 100)
-BLUE_HIGHLIGHT = (50, 50, 240, 100)
+# ROW and COL are reversed sometimes, this should be fixed
+WIN_SIZE = 1700
+BORDER_SCALE = 0.16
+TILE_SIZE = WIN_SIZE * 0.15714
+KING_SCALE = TILE_SIZE * 1.2727
+COL_SPACING = WIN_SIZE * 0.09714
+ROW_SPACING = WIN_SIZE * 0.09285
+KING_COL_ADJ = WIN_SIZE * 0.02343
+KING_ROW_ADJ = WIN_SIZE * 0.04270
+TILE_COL_OFFSET = WIN_SIZE * 0.12857
+TILE_ROW_OFFSET = WIN_SIZE * 0.09285
+RED_HIGHLIGHT = (240, 50, 50, 80)
+BLUE_HIGHLIGHT = (50, 50, 240, 80)
 RED_TILE = None
 BLUE_TILE = None
 
 BLK = (0, 0, 0)
 BGCOLOR = BLK
 board_image = pygame.image.load('./assets/brandubh_board.png')
+board_image = pygame.transform.scale(board_image, (WIN_SIZE, WIN_SIZE))
 viking_white = pygame.image.load('./assets/viking_white.png')
 viking_white = pygame.transform.scale(viking_white, (TILE_SIZE, TILE_SIZE))
 viking_black = pygame.image.load('./assets/viking_black.png')
 viking_black = pygame.transform.scale(viking_black, (TILE_SIZE, TILE_SIZE))
 viking_king = pygame.image.load('./assets/viking_king.png')
-viking_king = pygame.transform.scale(viking_king, (140, 140))
+viking_king = pygame.transform.scale(viking_king, (KING_SCALE, KING_SCALE))
 encoding_to_img = {1: viking_black,
                    2: viking_white,
                    3: viking_king,
@@ -55,18 +64,20 @@ def refresh(board: np.array,
     """
     display_rect = display.get_rect()
     display.blit(board_image, display_rect)
+    piece_rect = pygame.Rect((WIN_SIZE * BORDER_SCALE, WIN_SIZE * BORDER_SCALE, TILE_SIZE, TILE_SIZE))
+    display.blit(RED_TILE, piece_rect)
     for i, row in enumerate(range(board.shape[0])):
         for j, col in enumerate(range(board.shape[1])):
             piece = board[i, j].item()
-            cc = 90
-            rc = 65
+            cc = TILE_COL_OFFSET
+            rc = TILE_ROW_OFFSET
             if piece == 0 or piece == 4:
                 continue
             elif piece == 3:
-                cc -= 15
-                rc -= 30
+                cc -= KING_COL_ADJ
+                rc -= KING_ROW_ADJ
 
             piece_sprite = encoding_to_img[board[i, j].item()]
-            piece_rect = pygame.Rect((col * 68 + cc, row * 65 + rc, TILE_SIZE, TILE_SIZE))
+            piece_rect = pygame.Rect((col * COL_SPACING + cc, row * ROW_SPACING + rc, TILE_SIZE, TILE_SIZE))
             display.blit(piece_sprite, piece_rect)
     pygame.display.update()
